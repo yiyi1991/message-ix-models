80c26d64a7f67814dc0d42cfc4e50a055eb68a67
Florian Maczek <macflo@gmx.at>
2024-07-30 13:11:05 +0200

    Clean up build.py after rebase

--- message_ix_models/model/material/build.py
+++ message_ix_models/model/material/build.py
@@ -117,6 +117,7 @@ DATA_FUNCTIONS_2 = [
     gen_data_aluminum,
 ]
 
+# add as needed/implemented
 SPEC_LIST = [
     "generic",
     "common",
@@ -130,63 +131,6 @@ SPEC_LIST = [
     "methanol",
 ]
 
-
-# Try to handle multiple data input functions from different materials
-def add_data_1(scenario, dry_run=False):
-    """Populate `scenario` with MESSAGEix-Materials data."""
-    # Information about `scenario`
-    info = ScenarioInfo(scenario)
-
-    # Check for two "node" values for global data, e.g. in
-    # ixmp://ene-ixmp/CD_Links_SSP2_v2.1_clean/baseline
-    if {"World", "R11_GLB"} < set(info.set["node"]):
-        log.warning("Remove 'R11_GLB' from node list for data generation")
-        info.set["node"].remove("R11_GLB")
-    if {"World", "R12_GLB"} < set(info.set["node"]):
-        log.warning("Remove 'R12_GLB' from node list for data generation")
-        info.set["node"].remove("R12_GLB")
-
-    for func in DATA_FUNCTIONS_1:
-        # Generate or load the data; add to the Scenario
-        log.info(f"from {func.__name__}()")
-        data = func(scenario)
-        # if "SSP_dev" in scenario.model:
-        #     if "emission_factor" in list(data.keys()):
-        #         data.pop("emission_factor")
-        add_par_data(scenario, data, dry_run=dry_run)
-
-    log.info("done")
-
-
-def add_data_2(scenario, dry_run=False):
-    """Populate `scenario` with MESSAGEix-Materials data."""
-    # Information about `scenario`
-    info = ScenarioInfo(scenario)
-
-    # Check for two "node" values for global data, e.g. in
-    # ixmp://ene-ixmp/CD_Links_SSP2_v2.1_clean/baseline
-    if {"World", "R11_GLB"} < set(info.set["node"]):
-        log.warning("Remove 'R11_GLB' from node list for data generation")
-        info.set["node"].remove("R11_GLB")
-    if {"World", "R12_GLB"} < set(info.set["node"]):
-        log.warning("Remove 'R12_GLB' from node list for data generation")
-        info.set["node"].remove("R12_GLB")
-
-    for func in DATA_FUNCTIONS_2:
-        # Generate or load the data; add to the Scenario
-        log.info(f"from {func.__name__}()")
-        # TODO: remove this once emission_factors are back in SSP_dev
-        data = func(scenario)
-        # if "SSP_dev" in scenario.model:
-        #     if "emission_factor" in list(data.keys()):
-        #         data.pop("emission_factor")
-        add_par_data(scenario, data, dry_run=dry_run)
-
-    log.info("done")
-
-
-def ellipsize(elements: List) -> str:
-    """Generate a short string representation of `elements`.
 
 # Try to handle multiple data input functions from different materials
 def add_data_1(scenario, dry_run=False):
@@ -251,126 +195,13 @@ def build(
     # Apply to the base scenario
     spec = get_spec()
 
-    if "water_supply" not in list(scenario.set("level")):
-        scenario.check_out()
-        # add missing water tecs
-        scenario.add_set("technology", "extract__freshwater_supply")
-        scenario.add_set("level", "water_supply")
-        scenario.add_set("commodity", "freshwater_supply")
-
-        water_dict = pd.read_excel(
-            package_data_path("material", "other", "water_tec_pars.xlsx"),
-            sheet_name=None,
-        )
-        for par in water_dict.keys():
-            scenario.add_par(par, water_dict[par])
-        scenario.commit("add missing water tecs")
+    maybe_add_water_tecs(scenario)
 
     apply_spec(scenario, spec, add_data_1, fast=True)  # dry_run=True
     if "SSP_dev" not in scenario.model:
         engage_updates._correct_balance_td_efficiencies(scenario)
         engage_updates._correct_coal_ppl_u_efficiencies(scenario)
         engage_updates._correct_td_co2cc_emissions(scenario)
-    # spec = None
-    # apply_spec(scenario, spec, add_data_2)
-    from message_ix_models import ScenarioInfo
-
-    s_info = ScenarioInfo(scenario)
-    nodes = s_info.N
-
-    # Adjust exogenous energy demand to incorporate the endogenized sectors
-    # Adjust the historical activity of the useful level industry technologies
-    # Coal calibration 2020
-    add_ccs_technologies(scenario)
-    if old_calib:
-        modify_demand_and_hist_activity(scenario)
-    else:
-        modify_baseyear_bounds(scenario)
-        last_hist_year = scenario.par("historical_activity")["year_act"].max()
-        modify_industry_demand(scenario, last_hist_year, iea_data_path)
-        add_new_ind_hist_act(scenario, [last_hist_year], iea_data_path)
-        add_elec_i_ini_act(scenario)
-        add_emission_accounting(scenario)
-
-        # scenario.commit("no changes")
-    add_coal_lowerbound_2020(scenario)
-    add_cement_bounds_2020(scenario)
-
-    # Market penetration adjustments
-    # NOTE: changing demand affects the market penetration
-    # levels for the enduse technologies.
-    # FIXME: context.ssp only works for SSP1/2/3 currently missing SSP4/5
-    calibrate_UE_gr_to_demand(
-        scenario,
-        s_info,
-        data_path=package_data_path("material"),
-        ssp="SSP2",
-        region=identify_nodes(scenario),
-    )
-    calibrate_UE_share_constraints(scenario, s_info)
-
-    # Electricity calibration to avoid zero prices for CHN.
-    if "R12_CHN" in nodes:
-        add_elec_lowerbound_2020(scenario)
-
-    # i_feed demand is zero creating a zero division error during MACRO calibration
-    scenario.check_out()
-    scenario.remove_set("sector", "i_feed")
-    scenario.commit("i_feed removed from sectors.")
-
-    df = scenario.par(
-        "bound_activity_lo",
-        filters={"node_loc": "R12_RCPA", "technology": "sp_el_I", "year_act": 2020},
-    )
-    scenario.check_out()
-    scenario.remove_par("bound_activity_lo", df)
-    scenario.commit("remove sp_el_I min bound on RCPA in 2020")
-
-    return scenario
-
-
-def get_spec() -> Mapping[str, ScenarioInfo]:
-    """Return the specification for materials accounting."""
-    require = ScenarioInfo()
-    add = ScenarioInfo()
-    remove = ScenarioInfo()
-
-    # Load configuration
-    # context = Context.get_instance(-1)
-    context = read_config()
-
-    # Update the ScenarioInfo objects with required and new set elements
-    for type in SPEC_LIST:
-        for set_name, config in context["material"][type].items():
-            # for cat_name, detail in config.items():
-            # Required elements
-            require.set[set_name].extend(config.get("require", []))
-
-            # Elements to add
-            add.set[set_name].extend(config.get("add", []))
-
-            # Elements to remove
-            remove.set[set_name].extend(config.get("remove", []))
-
-    return dict(require=require, add=add, remove=remove)
-
-
-def build(scenario: message_ix.Scenario, old_calib: bool) -> message_ix.Scenario:
-    """Set up materials accounting on `scenario`."""
-
-    # Get the specification
-    # Apply to the base scenario
-    spec = get_spec()
-
-    maybe_add_water_tecs(scenario)
-
-    apply_spec(scenario, spec, add_data_1)  # dry_run=True
-    if "SSP_dev" not in scenario.model:
-        engage_updates._correct_balance_td_efficiencies(scenario)
-        engage_updates._correct_coal_ppl_u_efficiencies(scenario)
-        engage_updates._correct_td_co2cc_emissions(scenario)
-    spec = None
-    apply_spec(scenario, spec, add_data_2)
     from message_ix_models import ScenarioInfo
 
     s_info = ScenarioInfo(scenario)
@@ -397,11 +228,15 @@ def build(scenario: message_ix.Scenario, old_calib: bool) -> message_ix.Scenario
     # Market penetration adjustments
     # NOTE: changing demand affects the market penetration
     # levels for the enduse technologies.
-    # Note: context.ssp doesnt work
+    # FIXME: context.ssp only works for SSP1/2/3 currently missing SSP4/5
     calibrate_UE_gr_to_demand(
-        scenario, data_path=private_data_path(), ssp="SSP2", region="R12"
+        scenario,
+        s_info,
+        data_path=package_data_path("material"),
+        ssp="SSP2",
+        region=identify_nodes(scenario),
     )
-    calibrate_UE_share_constraints(scenario)
+    calibrate_UE_share_constraints(scenario, s_info)
 
     # Electricity calibration to avoid zero prices for CHN.
     if "R12_CHN" in nodes:
